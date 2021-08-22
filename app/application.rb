c1f4ddf5656f4d2b12e14748dd2c4dc3b2897918
christopher <coronac511@gmail.com>
2021-08-22 18:08:19 -0400

    updated routes

--- app/application.rb
+++ app/application.rb
@@ -1,58 +1,62 @@
 class Application
 
   def call(env)
-    resp = Rack::Response.new
+    res = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path == '/services' && req.get?
+    if req.path == '/companies' && req.get?
+      companies = Company.all
+      return [
+        200, 
+        { 'Content-Type' => 'application/json' }, 
+        [ companies.to_json ]
+      ]
+
+    elsif req.path.match(/companies/) && req.post?
+      body = JSON.parse(req.body.read)
+      company = Company.create(body)
+      return [
+        200, 
+        { 'Content-Type' => 'application/json' }, 
+        [ company.to_json ]
+      ]
+
+    elsif req.path == '/services' && req.get?
       services = Service.all
       return [
-        200,
-         { 'Content-Type' => 'application/json' }, 
-         [ services.to_json ]
-        ]
-
-      elsif req.path.match(/companies/) && req.post?
-        body = JSON.parse(req.body.read)
-        company = Company.create(body)
+        200, 
+        { 'Content-Type' => 'application/json' }, 
+        [ services.to_json ]
+      ]
+    
+    elsif req.path.match(/services/) && req.get?
+      id = req.path.split('/')[2]
+      service = Service.find_by(id: id)
+      if service
+        companies = service.companies
+        service_res = {
+          service: service,
+          companies: companies
+        }
         return [
           200, 
           { 'Content-Type' => 'application/json' }, 
-          [ company.to_json ]
+          [ service_res.to_json ]
         ]
-
-    elsif req.path == '/companies' && req.get?
-          companies = Company.all
-          return [
-            200,
-             { 'Content-Type' => 'application/json' }, 
-             [ companies.to_json ]
-            ]
-
-          elsif req.path.match(/companies/) && req.get?
-            id = req.path.split('/')[2]
-            company = Company.find_by(id: id)
-            if company
-              
-              return [
-                200, 
-                { 'Content-Type' => 'application/json' }, 
-                [ company.to_json ]
-              ]
-            else
-              return [
-                204, 
-                { 'Content-Type' => 'application/json' }, 
-                [ { error: 'Company not found' }.to_json ]
-              ]
-            end
+      else
+        return [
+          204, 
+          { 'Content-Type' => 'application/json' }, 
+          [ { error: 'service not found' }.to_json ]
+        ]
+      end
 
     else
-      resp.write "Path Not Found"
-     
+      res.write "Path Not Found"
+
     end
 
-    resp.finish
+    res.finish
   end
 
 end
